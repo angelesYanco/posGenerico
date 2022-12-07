71e2324058d22381c2a49577c65a474f3481752a
Antonio <angeles_yanco@hotmail.com>
2022-12-06 19:51:45 -0600

    Agregando modulos parte 1 completa y 2 parcial.

--- vistas/modulos/cabecera.php
+++ vistas/modulos/cabecera.php
@@ -1,3 +1,39 @@
 <header class="main-header">
 
+    <!-- *****************************
+    LOGOTIPO
+    ******************************* -->
+
+    <a href="" class="logo">
+
+        <!-- logo mini -->
+        <span class="logo-mini">
+
+            <img src="vistas/img/plantilla/icono-blanco.png" alt="" 
+            class="img-responsive" style="padding:10px">
+        </span>
+
+        <!-- logo normal -->
+        <span class="logo-mini">
+
+            <img src="vistas/img/plantilla/icono-blanco-lineal.png" alt="" 
+            class="img-responsive" style="padding:10px 0px">
+        </span>
+
+    </a>
+
+    <!-- *****************************
+    SIDEBAR: BARRA DE NAVEGACION
+    ******************************* -->
+    <nav class="navbar navbar-static-top" role="navigation">
+
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button"></a>
+
+        <span class="sr-only">Toggle navigation</span>
+        <!-- <span class=""></span>
+        <span class=""></span>
+        <span class=""></span> -->
+        
+    </nav>
+
 </header>
