7709a4224743c1103dae70b3ebdf491f3be23ca3
apangeles <angeles_yanco@hotmail.com>
2022-12-06 23:42:27 -0600

    28. Agregando modulos parte 2, terminado.

--- vistas/modulos/cabecera.php
+++ vistas/modulos/cabecera.php
@@ -14,9 +14,9 @@
         </span>
 
         <!-- logo normal -->
-        <span class="logo-mini">
+        <span class="logo-lg">
 
-            <img src="vistas/img/plantilla/icono-blanco-lineal.png" alt="" 
+            <img src="vistas/img/plantilla/logo-blanco-lineal.png" alt="" 
             class="img-responsive" style="padding:10px 0px">
         </span>
 
@@ -26,14 +26,41 @@
     SIDEBAR: BARRA DE NAVEGACION
     ******************************* -->
     <nav class="navbar navbar-static-top" role="navigation">
-
+        
+        <!-- Boton denavegación -->
         <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button"></a>
 
         <span class="sr-only">Toggle navigation</span>
-        <!-- <span class=""></span>
-        <span class=""></span>
-        <span class=""></span> -->
-        
+
+        <!-- Perfil de usuario -->
+
+        <div class="navbar-custom-menu">
+
+            <ul class="nav navbar-nav">
+                <li class="dropdown user user-menu">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+
+                        <img src="vistas/img/usuarios/default/anonymous.png" class="user-image">
+
+                        <span class="hidden-xs">Usuario Administrador</span>
+                    </a>
+
+                    <!-- Dropdown -->
+                    <ul class="dropdown-menu">
+
+                        <li class="user-body">
+
+                                <a href="" class="btn btn-default btn-flat">Salir</a>
+
+                        </li>
+
+                    </ul>
+
+                </li>
+            </ul>
+
+        </div>
+
     </nav>
 
 </header>
