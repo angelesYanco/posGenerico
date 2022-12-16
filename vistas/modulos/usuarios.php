204ffe80376828b16c8511577b558e4482d64e30
Antonio <angeles_yanco@hotmail.com>
2022-12-15 19:48:54 -0600

    37. Maquetando el módulo de usuarios-parte2: terminado.

--- vistas/modulos/usuarios.php
+++ vistas/modulos/usuarios.php
@@ -11,7 +11,7 @@
   </section>
 
   <!-- Main content 
-  Cuadricula con los datos de los usuarios
+  Tabla con los datos de los usuarios
   -->
   <section class="content">
 
@@ -57,32 +57,108 @@
           </tbody>
         </table>
       </div>
+      
     </div>
     <!-- /.box -->
   </section>
   <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
-<!--==================================== 
-Ventana modal para agregar usuario
-=====================================-->
-<!-- Modal -->
+
+<!--
+  Ventana modal para agregar usuario
+-->
 <div id="modalAgregarUsuario" class="modal fade" role="dialog">
   <div class="modal-dialog">
-
-    <!-- Modal content-->
     <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header</h4>
-      </div>
-      <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-      </div>
-    </div>
+      <form role="form" method="post" enctype="multipart/form-data">
+        <div class="modal-header" style="background:#3c8dbc; color:white">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Agregar usuario</h4>
+        </div>
 
+        <div class="modal-body">        
+          <div class="box-body">
+          <!-- 
+            Entrada para datos personales 
+          -->
+            <!-- Nombre -->
+            <div class="form-group">            
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                <input type="text" class="form-control input-lg" name="nuevoNombre" placeholder="Nombre" required>
+              </div>
+            </div>
+
+            <!-- Apellido Paterno -->
+            <div class="form-group">
+              <div class="input-group">
+                  <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                  <input type="text" class="form-control input-lg" name="nuevoApellidoPaterno" placeholder="Apellido paterno" required>
+              </div>
+            </div>
+
+            <!-- Apellido Materno -->
+            <div class="form-group">
+              <div class="input-group">    
+                  <span class="input-group-addon"><i class="fa fa-user"></i></span>
+                  <input type="text" class="form-control input-lg" name="nuevoApellidoMaterno" placeholder="Apellido materno" required>
+              </div>
+            </div>  
+
+          <!-- 
+            Entrada para datos en el sistema 
+          -->
+            <div class="form-group">
+              <div class="input-group">
+                <!-- Usuario -->
+                <span class="input-group-addon"><i class="fa fa-key"></i></span>
+                <input type="text" class="form-control input-lg" name="nuevoUsuario" placeholder="Usuario" required>
+                <!-- Password -->
+                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+                <input type="password" class="form-control input-lg" name="nuevoPassword" placeholder="Contraseña" required>
+              </div>
+            </div>
+
+            <!-- 
+              Entrada para seleccion de perfil 
+            -->
+            <div class="form-group">
+              <div class="input-group">
+                <span class="input-group-addon"><i class="fa fa-users"></i></span>
+                <select class="form-control input-lg" name="nuevoPerfil">
+                  <option value="">Seleccionar perfil</option>
+                  <option value="Administrador">Administrador</option>
+                  <option value="Especial">Especial</option>
+                  <option value="Vendedor">Vendedor</option>
+                </select>
+              </div>
+            </div>
+            <!-- 
+              Entrada para seleccion de perfil 
+            -->
+            <div class="form-group">
+              <!-- <div class="panel">SUBIR FOTO
+                <input type="file" id="nuevaFoto" name="nuevaFoto">
+                <p class="help-block">Peso máximo de la foto: 200 MB</p>
+                <img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width="100px">
+              </div> -->
+              <input type="file" id="nuevaFoto" name="nuevaFoto">
+              <p class="help-block">
+                <img src="vistas/img/usuarios/default/anonymous.png" class="img-thumbnail" width="60px">
+                Peso máximo de la foto: 200 MB
+              </p>
+            </div>
+        </div>
+        <!-- 
+          Footer
+        -->
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+          <button type="submit" class="btn btn-primary">Guardar usuario</button>
+        </div>
+      </form>
+    </div>
   </div>
 </div>
+
