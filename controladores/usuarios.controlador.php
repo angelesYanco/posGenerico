6084b392a524d4d52e59029c2066aa5b749e42a0
Antonio <angeles_yanco@hotmail.com>
2023-01-03 19:27:25 -0600

    49. Registrar ultimo login: codificado, pruebas satisfactorias, terminado.

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -30,11 +30,31 @@ class ControladorUsuarios{
                         $_SESSION["usuario"] = $respuesta["usuario"];
                         $_SESSION["foto"] = $respuesta["foto"];
                         $_SESSION["perfil"] = $respuesta["perfil"];
-    
-                        echo '<script>
+
+                        //Actualizar ultimo login
+                        date_default_timezone_set('America/Mexico_City');
+                        
+                        $fecha = date('Y-m-d');
+                        $hora = date('H:i:s');
+
+                        $fechaActual = $fecha.' '.$hora;
+
+                        $item1 = 'fecha_ultimo_login';
+                        $valor1 = $fechaActual;
+
+                        $item2 = 'id_usuario';
+                        $valor2 = $respuesta["id_usuario"];
+
+                        $ultimoLogin = ModeloUsuarios::mdlActualizarUsuario($tabla, $item1, $valor1, $item2, $valor2);
+
+                        if($ultimoLogin == 'ok'){
+
+                            echo '<script>
                         
                             window.location = "inicio";
-                        </script>';
+                            </script>';                            
+                        }
+
                     }else{
 
                         echo '<br><div class="alert alert-danger">El usuario no esta activo en el sistema!</div>';
